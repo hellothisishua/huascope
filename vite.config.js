c05e3066bb15dc72f8d0ab22ea567495ee3f35cc
hellothisishua <user@example.com>
2026-08-10 15:08:39 +0800

    Fix base path back to /huascope/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/huascope/',
   define: {
     'import.meta.env.VITE_TMDB_KEY': JSON.stringify('02c952df054afb8ca11440a0f84b080a'),
   },
